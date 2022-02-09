93aabd4a6c2abc32152c86a8836079a4f21b6d9e
AntonellaSannino <antonella.sannino001@studenti.uniparthenope.it>
2022-02-09 19:37:39 +0100

    App.py

--- app.py
+++ app.py
@@ -1,7 +1,9 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, flash
 from flask_bootstrap import Bootstrap
 
 app = Flask(__name__)
+
+app.config['SECRET_KEY']='BNRD gnmldesw HUgI HSNsgs'
 
 bootstrap = Bootstrap(app)
 
@@ -15,23 +17,65 @@ def about():
     return render_template('aboutus.html')
 
 
-@app.route("/login")
+@app.route("/login", methods=['GET','POST'])
 def login():
-    return render_template('login.html')
+    return render_template('login.html', boolean=True)
 
 
-@app.route("/signin")
+@app.route("/signin", methods=['GET','POST'])
 def signin():
+    if request.method == 'POST':
+        name= request.form.get('Name')
+        surname = request.form.get('Surname')
+        address = request.form.get('Address')
+        date = request.form.get('Date')
+        gender = request.form.get('Gender')
+        telephone = request.form.get('Telephone')
+        taxcode= request.form.get('Taxcode')
+        email = request.form.get('Email')
+        password = request.form.get('Password')
+        flash('Account creato!', category="success")
+
     return render_template('signin.html')
 
 
-@app.route("/signinlocal")
+@app.route("/signinlocal", methods=['GET','POST'])
 def signinlocal():
+    if request.method == 'POST':
+        name= request.form.get('Name')
+        surname = request.form.get('Surname')
+        address = request.form.get('Address')
+        date = request.form.get('Date')
+        gender = request.form.get('Gender')
+        telephone = request.form.get('Telephone')
+        taxcode= request.form.get('Taxcode')
+        document = request.form.get('Document')
+        iban = request.form.get('Iban')
+        iva = request.form.get('Iva')
+        localname = request.form.get('LocalName')
+        email = request.form.get('Email')
+        password = request.form.get('Password')
+
+
     return render_template('signinlocal.html')
 
 
-@app.route("/signinrider")
+@app.route("/signinrider", methods=['GET','POST'])
 def signinrider():
+    if request.method == 'POST':
+        name = request.form.get('Name')
+        surname = request.form.get('Surname')
+        address = request.form.get('Address')
+        date = request.form.get('Date')
+        gender = request.form.get('Gender')
+        telephone = request.form.get('Telephone')
+        taxcode = request.form.get('Taxcode')
+        document = request.form.get('Document')
+        iban = request.form.get('Iban')
+        email = request.form.get('Email')
+        password = request.form.get('Password')
+
+
     return render_template('signinrider.html')
 
 
