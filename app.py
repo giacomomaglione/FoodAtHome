2af3e27f5f6904525088e03183276d6268a4eeb5
Pasquale Sannino <pasquale.sannino3@studenti.uniparthenope.it>
2022-02-09 17:45:56 +0100

    Gestite le richieste e pagine web dinamiche con Jinja2

--- app.py
+++ app.py
@@ -1,16 +1,40 @@
-from flask import Flask
+from flask import Flask, render_template
+from flask_bootstrap import Bootstrap
 
 app = Flask(__name__)
 
+bootstrap = Bootstrap(app)
 
 @app.route("/")
 def index():
-    return "Hello Worldss!"
+    return render_template('index.html')
 
 
-@app.route("/about")
+@app.route("/aboutus")
 def about():
-    return "<h1 style='color: red'>informazioni</h1>"
+    return render_template('aboutus.html')
+
+
+@app.route("/login")
+def login():
+    return render_template('login.html')
+
+
+@app.route("/signin")
+def signin():
+    return render_template('signin.html')
+
+
+@app.route("/signinlocal")
+def signinlocal():
+    return render_template('signinlocal.html')
+
+
+@app.route("/signinrider")
+def signinrider():
+    return render_template('signinrider.html')
+
+
 
 
 if __name__ == "__main__":
