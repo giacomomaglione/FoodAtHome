18f56d9eb5a45e6a5028997fd405487b1c7b24e7
AlessioDattilo <alessio.dattilo001@studenti.uniparthenope.it>
2022-02-09 15:52:58 +0100

    Function JavaScript

--- function.js
+++ function.js
@@ -115,3 +115,94 @@ function upperCase() {
     iv.value = iv.value.toUpperCase();
 }
 
+
+function viewNewOrder() {
+    var div = document.getElementById("NewOrder");
+    if (div.style.display === "none") {
+        div.style.display = "block";
+    } else {
+        div.style.display = "none";
+    }
+}
+
+
+function viewMenuControl() {
+    var div_menu = document.getElementById("MenuControl");
+    var div_order = document.getElementById("OrderControl");
+    if (div_menu.style.display === "none") {
+        div_menu.style.display = "block";
+        div_order.style.display = "none";
+    } else {
+        div_menu.style.display = "none";
+    }
+}
+function viewOrderControl() {
+    var div_menu = document.getElementById("MenuControl");
+    var div_order = document.getElementById("OrderControl");
+    if (div_order.style.display === "none") {
+        div_order.style.display = "block";
+        div_menu.style.display = "none";
+    } else {
+        div_order.style.display = "none";
+    }
+}
+
+
+function viewInsertProduct() {
+    var div_ins = document.getElementById("InsertProduct");
+    var div_rem = document.getElementById("RemoveProduct");
+    var div_mod = document.getElementById("ModifyProduct");
+    if (div_ins.style.display === "none") {
+        div_ins.style.display = "block";
+        div_rem.style.display = "none";
+        div_mod.style.display = "none";
+    } else {
+        div_ins.style.display = "none";
+    }
+}
+function viewModifyProduct() {
+    var div_ins = document.getElementById("InsertProduct");
+    var div_rem = document.getElementById("RemoveProduct");
+    var div_mod = document.getElementById("ModifyProduct");
+    if (div_mod.style.display === "none") {
+        div_mod.style.display = "block";
+        div_ins.style.display = "none";
+        div_rem.style.display = "none";
+    } else {
+        div_mod.style.display = "none";
+    }
+}
+function viewRemoveProduct() {
+    var div_ins = document.getElementById("InsertProduct");
+    var div_rem = document.getElementById("RemoveProduct");
+    var div_mod = document.getElementById("ModifyProduct");
+    if (div_rem.style.display === "none") {
+        div_rem.style.display = "block";
+        div_ins.style.display = "none";
+        div_mod.style.display = "none";
+    } else {
+        div_rem.style.display = "none";
+    }
+}
+
+
+function viewShiftPage(){
+    var div_shi = document.getElementById("ShiftPage");
+    var div_ord = document.getElementById("OrderPage");
+    if (div_shi.style.display === "none") {
+        div_shi.style.display = "block";
+        div_ord.style.display = "none";
+    } else {
+        div_shi.style.display = "none";
+    }
+}
+function viewOrderPage(){
+    var div_shi = document.getElementById("ShiftPage");
+    var div_ord = document.getElementById("OrderPage");
+    if (div_ord.style.display === "none") {
+        div_ord.style.display = "block";
+        div_shi.style.display = "none";
+    } else {
+        div_ord.style.display = "none";
+    }
+}
