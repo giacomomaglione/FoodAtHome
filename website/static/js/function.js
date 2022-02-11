74b68e68d50934e35adca71ee715565737969cda
GiacomoMaglione <giacomo.maglione001@studenti.uniparthenope.it>
2022-02-11 20:15:39 +0100

    Javascript Function File

--- website/static/js/function.js
+++ website/static/js/function.js
@@ -177,30 +177,3 @@ function viewOrderPage(){
         div_ord.style.display = "none";
     }
 }
-
-
-function searchLocal(){
-    let table = document.createElement('table');
-    let thead = document.createElement('thead');
-    let tbody = document.createElement('tbody');
-
-    table.appendChild(thead);
-    table.appendChild(tbody);
-
-    let row_1 = document.createElement('tr');
-    let heading_1 = document.createElement('th');
-    heading_1.innerHTML = "Nome locale";
-
-    row_1.appendChild(heading_1);
-    thead.appendChild(row_1);
-
-    for(i=0;i<2;i++){
-        let row_2 = document.createElement('tr');
-        let row_2_data_1 = document.createElement('td');
-        row_2_data_1.innerHTML = "1.";
-
-        row_2.appendChild(row_2_data_1);
-        tbody.appendChild(row_2);
-
-    }
-}
