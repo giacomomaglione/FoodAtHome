0cbf9f5674cfa2cf42c0fc40b13c2fe9938a9e77
GiacomoMaglione <giacomo.maglione001@studenti.uniparthenope.it>
2022-02-11 12:24:35 +0100

    Database Python File

--- website/database.py
+++ website/database.py
@@ -3,19 +3,12 @@ import pymongo
 
 class Database(object):
     URI = "mongodb+srv://foodathome:UniParthenope@cluster0.fkbq2.mongodb.net/test"
-
     DATABASE = None
-
 
     @staticmethod
     def initialize():
         client = pymongo.MongoClient(Database.URI, tlsCAFile = certifi.where())
         Database.DATABASE = client['foodathome']
-
-
-    #cliente = DATABASE["Customer"]
-    #rider = DATABASE["Rider"]
-    #negozio = DATABASE["Store"]
 
     @staticmethod
     def insert(collection, data):
