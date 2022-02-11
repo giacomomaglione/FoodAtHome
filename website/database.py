e3368659d58a813af8c0dbe6c3d661c8c86b114e
AntonellaSannino <antonella.sannino001@studenti.uniparthenope.it>
2022-02-11 19:27:17 +0100

    Python File

--- website/database.py
+++ website/database.py
@@ -1,6 +1,8 @@
 import certifi
 import pymongo
 
+
+
 class Database(object):
     URI = "mongodb+srv://foodathome:UniParthenope@cluster0.fkbq2.mongodb.net/test"
 
@@ -13,18 +15,4 @@ class Database(object):
         Database.DATABASE = client['foodathome']
 
 
-    #cliente = DATABASE["Customer"]
-    #rider = DATABASE["Rider"]
-    #negozio = DATABASE["Store"]
 
-    @staticmethod
-    def insert(collection, data):
-        Database.DATABASE[collection].insert(data)
-
-    @staticmethod
-    def find(collection, query):
-        return Database.DATABASE[collection].find(query)
-
-    @staticmethod
-    def find_one(collection, query):
-        return Database.DATABASE[collection].find_one(query)
