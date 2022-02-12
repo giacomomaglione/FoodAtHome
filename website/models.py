d98932955b11b7b718e8510b57bfbd2f2abb5622
AntonellaSannino <antonella.sannino001@studenti.uniparthenope.it>
2022-02-12 12:38:22 +0100

    Python File

--- website/models.py
+++ website/models.py
@@ -28,3 +28,30 @@ class Cliente():
             return None
         return cliente(username=u['username'])
 
+
+
+class Rider():
+    def __init__(self, username):
+        self.username = username
+
+    @staticmethod
+    def is_authenticated():
+        return True
+
+    @staticmethod
+    def is_active():
+        return True
+
+    @staticmethod
+    def is_anonymous():
+        return False
+
+    def get_id(self):
+        return self.username
+
+    @login.user_loader
+    def load_user(username):
+        u = cliente.find_one({"Name": username})
+        if not u:
+            return None
+        return cliente(username=u['username'])
