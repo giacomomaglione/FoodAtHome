be80e1b4cb8a4e41a4a79e49d09afa8fcdc8922f
Pasquale Sannino <pasquale.sannino3@studenti.uniparthenope.it>
2022-02-12 03:51:23 +0100

    Fixed User Auth

--- website/auth.py
+++ website/auth.py
@@ -1,60 +1,38 @@
-from flask import Blueprint, request, flash, render_template, redirect, url_for, session, redirect
+from flask import Blueprint, redirect
 from flask import render_template, url_for, request, flash
-from flask_login import LoginManager
-from flask_login import current_user, login_user, logout_user, login_required
-
-#from database import Database
-from . import cliente, rider, negozio
-
+from flask_login import current_user, login_user, logout_user, login_required, UserMixin
+from .form import Login
+from werkzeug.urls import url_parse
+from . import cliente
+from .models import Cliente # NON LO CANCELLARE; Missing user_loader or request_loader
 
 auth = Blueprint('auth', __name__)
 
-class Cliente:
-    def __init__(self, username):
-        self.username = username
-
-    @staticmethod
-    def is_authenticated():
-        return True
-
-    @staticmethod
-    def is_active():
-        return True
-
-    @staticmethod
-    def is_anonymous():
-        return False
-
-    def get_id(self):
-        return self.username
-
 @auth.route("/login", methods=['GET', 'POST'])
 def login():
-    if request.method == 'POST':
-        email = request.form.get('Email')
-        password = request.form.get('Password')
-
-        user = cliente.find_one({"Email": email})
-
-        if user is None:
-            flash('Email non registrata', category="error")
+    if current_user.is_authenticated:
+        return redirect(url_for('index'))
+    form = Login()
+    if form.validate_on_submit():
+        user = cliente.find_one({"Email": request.form.get('Email')})
+        if user and user["Password"] == request.form.get('Email'):
+            user_obj = Cliente(username=user['Email'])
+            login_user(user_obj)
+            next_page = request.args.get('next')
+            if not next_page or url_parse(next_page).netloc != '':
+                next_page = url_for('index')
+            return redirect(next_page)
         else:
-            if user["Email"] == email:
-                if user["Password"] == password:
-                    flash('Accesso eseguito', category="success")
-                    # login_user(user, remember=True)
-                    # return redirect(url_for('views.clentindex'))
-                else:
-                    flash('Password incorretta', category="error")
-
-    return render_template('login.html', boolean=True)
+            flash("Invalid username or password")
+    else:
+        flash("validate_on_submit")
+    return render_template('login.html', form = form)
 
 @auth.route("/logout")
-#@login_required
+@login_required
 def logout():
     logout_user()
     return redirect(url_for('views.index'))
-
 
 @auth.route("/signin", methods=['GET', 'POST'])
 def signin():
@@ -91,117 +69,3 @@ def signin():
             flash("Email già registrata", category="error")
 
     return render_template('signin.html')
-
-
-@auth.route("/loginrider", methods=['GET', 'POST'])
-def loginrider():
-    if request.method == 'POST':
-        email = request.form.get('Email')
-        password = request.form.get('Password')
-
-        user = rider.find_one({"Email": email})
-
-        if user is None:
-            flash('Email non registrata', category="error")
-        else:
-            if user["Email"] == email:
-                if user["Password"] == password:
-                    flash('Accesso eseguito', category="success")
-                    login_user(user, remember=True)
-                    return redirect(url_for('views.riderindex'))
-                else:
-                    flash('Password incorretta', category="error")
-
-    return render_template('login.html', boolean=True)
-
-
-@auth.route("/loginshop", methods=['GET', 'POST'])
-def loginshop():
-    if request.method == 'POST':
-        email = request.form.get('Email')
-        password = request.form.get('Password')
-
-        user = negozio.find_one({"Email": email})
-
-        if user is None:
-            flash('Email non registrata', category="error")
-        else:
-            if user["Email"] == email:
-                if user["Password"] == password:
-                    flash('Accesso eseguito', category="success")
-                    login_user(user, remember=True)
-                    return redirect(url_for('views.localindex'))
-                else:
-                    flash('Password incorretta', category="error")
-
-    return render_template('login.html', boolean=True)
-
-
-@auth.route("/signinlocal", methods=['GET', 'POST'])
-def signinlocal():
-    if request.method == 'POST':
-        name = request.form.get('Name')
-        surname = request.form.get('Surname')
-        street = request.form.get('Street')
-        city = request.form.get('City')
-        province = request.form.get('Province')
-        date = request.form.get('Date')
-        gender = request.form.get('Gender')
-        telephone = request.form.get('Telephone')
-        taxcode = request.form.get('Taxcode')
-        document = request.form.get('Document')
-        iban = request.form.get('Iban')
-        iva = request.form.get('Iva')
-        localname = request.form.get('LocalName')
-        email = request.form.get('Email')
-        password = request.form.get('Password')
-
-        if len(email) < 4:
-            flash("L'email deve essere di almeno 4 caratteri!", category="error")
-        elif len(name) < 3:
-            flash("Il nome deve essere di almeno 3 caratteri!", category="error")
-        elif len(password) < 7:
-            flash("La password deve essere di almeno 7 caratteri!", category="error")
-        else:
-            flash('Account creato!', category="success")
-
-        account = {"Name": name, "Surname": surname, "Street": street, "City": city, "Province": province,
-                   "Birthday": date, "Gender": gender, "PhoneNumber": telephone, "TaxCode": taxcode, "ID": document,
-                   "IBAN": iban, "IVA": iva, "LocalName": localname, "Email": email, "Password": password}
-        negozio.insert_one(account)
-
-    return render_template('signinlocal.html')
-
-
-@auth.route("/signinrider", methods=['GET', 'POST'])
-def signinrider():
-    if request.method == 'POST':
-        name = request.form.get('Name')
-        surname = request.form.get('Surname')
-        street = request.form.get('Street')
-        city = request.form.get('City')
-        province = request.form.get('Province')
-        date = request.form.get('Date')
-        gender = request.form.get('Gender')
-        telephone = request.form.get('Telephone')
-        taxcode = request.form.get('Taxcode')
-        document = request.form.get('Document')
-        iban = request.form.get('Iban')
-        email = request.form.get('Email')
-        password = request.form.get('Password')
-
-        if len(email) < 4:
-            flash("L'email deve essere di almeno 4 caratteri!", category="error")
-        elif len(name) < 3:
-            flash("Il nome deve essere di almeno 3 caratteri!", category="error")
-        elif len(password) < 7:
-            flash("La password deve essere di almeno 7 caratteri!", category="error")
-        else:
-            flash('Account creato!', category="success")
-
-        account = {"Name": name, "Surname": surname, "Street": street, "City": city, "Province": province,
-                   "Birthday": date, "Gender": gender, "PhoneNumber": telephone, "TaxCode": taxcode, "ID": document,
-                   "IBAN": iban, "Email": email, "Password": password}
-        rider.insert_one(account)
-
-    return render_template('signinrider.html')
