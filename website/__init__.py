214b351917c8d0f135db85c0d4535bc85b5f22cf
Pasquale Sannino <pasquale.sannino3@studenti.uniparthenope.it>
2022-02-12 03:48:03 +0100

    Fixed LoginManager

--- website/__init__.py
+++ website/__init__.py
@@ -1,29 +1,31 @@
+from flask import Flask
 from website import database
-from flask import Flask, render_template, session, redirect
-from flask_session import Session
-
-from functools import wraps
+from flask_login import LoginManager
 
 database.Database.initialize()
 
-cliente= database.Database.DATABASE["Customer"]
+cliente = database.Database.DATABASE["Customer"]
 rider = database.Database.DATABASE["Rider"]
 negozio = database.Database.DATABASE["Store"]
+
+login = LoginManager()
+login.login_view = 'login'
 
 def create_app():
     app = Flask(__name__)
     app.config['SECRET_KEY'] = 'BNRD gnmldesw HUgI HSNsgs'
-    app.secret_key='DUIJSF98RDBFnsjeurndp'
-    app.config["SESSION_PERMANENT"]= False
+    app.secret_key ='DUIJSF98RDBFnsjeurndp'
+    app.config["SESSION_PERMANENT"] = False
     app.config["SESSION_TYPE"] = "filesystem"
-
-
-
 
     from .views import views
     from .auth import auth
+    from .form import form
+
+    login.init_app(app)
 
     app.register_blueprint(views, url_prefix='/')
     app.register_blueprint(auth, url_prefix='/')
+    app.register_blueprint(form, url_prefix='/')
 
     return app
