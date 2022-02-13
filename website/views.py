6a8555452ddacda053c0099680446549b1595dd4
AntonellaSannino <antonella.sannino001@studenti.uniparthenope.it>
2022-02-13 13:26:49 +0100

    Python File

--- website/views.py
+++ website/views.py
@@ -1,5 +1,10 @@
-from flask import Blueprint, render_template
-from flask_login import login_required
+from flask import Blueprint, render_template, request
+from flask_login import login_required, current_user, login_user
+from .form import EditProfile
+from . import cliente, rider, negozio
+from .models import Cliente, Rider, Local
+from . import login
+
 
 views = Blueprint('views', __name__)
 
@@ -21,6 +26,54 @@ def customerindex():
 @login_required
 def customereditprofile():
     return render_template('customereditprofile.html')
+
+@views.route("/editprofile", methods=['GET', 'POST'])
+@login_required
+def editprofile():
+    form = EditProfile()
+    if request.method == 'POST':
+        name = form.name.data
+        surname = form.surname.data
+        street = form.street.data
+        city = form.city.data
+        province = form.province.data
+        date = form.date.data
+        gender = form.gender.data
+        telephone = form.telephone.data
+        taxcode = form.taxcode.data
+        email = form.email.data
+        password = form.password.data
+        id = form.id.data
+        iban = form.iban.data
+        localname = form.localname.data
+        piva = form.piva.data
+
+        if current_user.type == 0 :
+            utente = cliente.find_one(current_user['Email'])
+            if name is None :
+                utente = cliente.update({'Name': utente['Name']}, {"$set": name})
+
+
+
+
+
+
+
+
+
+
+
+
+
+    return render_template('editprofile.html' , form=form)
+
+
+
+
+
+
+
+    return render_template('editprofile.html')
 
 @views.route("/riderindex")
 @login_required
